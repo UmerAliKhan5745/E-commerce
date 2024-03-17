ac1ad6e30484e4deef25bf2785771506284b96bd
Umer Ali Khan <aumeralikhan@gamil.com>
2024-03-17 15:50:27 +0500

    last commit

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -3,16 +3,13 @@ import React from 'react'
 import Navbarr from './components/navbar/page'
 import MainSection from './components/mainsection/page'
 import Footer from './components/footer/page'
-import { store } from '@/store/store'
-import { Provider } from 'react-redux'
+
 function page() {
   return (
     <div>
-        <Provider store={store}>
       <Navbarr/>
       <MainSection/>
       <Footer/> 
-   </Provider>
       </div>
   )
 }
