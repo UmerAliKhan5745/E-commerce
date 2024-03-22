24a74d3a0c53747f5e0fb1b1765546e442f80dda
UmerAliKhan5745 <154968780+UmerAliKhan5745@users.noreply.github.com>
2024-03-22 04:25:45 -0700

    added RTK

--- Client/src/app/pages/cart/cartSidebar/page.tsx
+++ Client/src/app/pages/cart/cartSidebar/page.tsx
@@ -1,22 +1,53 @@
 "use client"
+
 import { useState } from 'react';
+import type { RootState } from '../../../../store'; // Assuming RootState is defined in ../../../../store
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../../../features/counter/counterSlice';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-function Cart() {
 
+export function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function Cart() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
   return (
     <>
       <FontAwesomeIcon icon={faCartShopping} style={{color:"white"}} onClick={handleShow}/>
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Shoping Cart</Offcanvas.Title>
+          <Offcanvas.Title>Shopping Cart</Offcanvas.Title> {/* Fixed title */}
         </Offcanvas.Header>
         <Offcanvas.Body>
+          <Counter /> {/* Render Counter component here */}
         </Offcanvas.Body>
       </Offcanvas>
     </>
